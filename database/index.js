3922cf87ae7d29eb80b2dacbe6ee9f94847f689d
Trevor Smith <smittrevor@gmail.com>
2019-02-07 11:22:16 -0600

    Establish connection to MongoDB hosted on Atlas

---
+++ database/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://tdsmith27:homerepot@cluster0-cawa1.mongodb.net/reviews?retryWrites=true');
+
+const reviewSchema = mongoose.Schema({
+  product_id: 'number',
+  author: 'string',
+  date: 'string',
+  rating: 'number',
+  review: 'string'
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+const save = (doc, cb) => {
+  let review = new Review(doc);
+
+  review.save()
+    .then(data => cb(null, data))
+    .catch(err => cb(err));
+};
+
+module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -4,8 +4,22 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3030;
 
+const db = require(path.join(__dirname, '../database/index.js'));
+
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(parser.json());
+
+app.post('/products/reviews', (req, res) => {
+
+  db.save(req.body, (err, data) => {
+    if (err) console.log('we got an error', err)
+    else {
+      console.log('yo this may have done just worked')
+    }
+  })
+  
+  res.sendStatus(201);
+}) 
 
 
 
