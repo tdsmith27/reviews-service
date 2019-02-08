f32e1b2b6f85fb1028eba6d6fe6f6a95ef67db2d
Trevor Smith <smittrevor@gmail.com>
2019-02-08 12:14:42 -0600

    Retrieve all reviews for current product on page load

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReviewsSummary from './ReviewsSummary.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      product_id: 5,
+      reviews: []
+    };
   }
+
+  componentDidMount() {
+    let url = `/products/${this.state.product_id}/reviews`;
+    axios.get(url)
+      .then(response => this.setState({reviews: response.data}))
+      .catch(err => console.log('error componentDidMount get request', err));
+  };
+
 
   render() {
     return (
