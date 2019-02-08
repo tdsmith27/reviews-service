c293310f997676d743c188f2662162ce9fc9cc95
Trevor Smith <smittrevor@gmail.com>
2019-02-08 15:49:41 -0600

    Format basic layout

--- client/components/ReviewsSummary.jsx
+++ client/components/ReviewsSummary.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+const boxStyle = {
+  borderRight: '1px solid #cccccc',
+  width: '30%',
+  display: 'inline-block',
+  marginLeft: '19%',
+  marginTop: '10px',
+  marginBottom: '10px',
+  float: 'left'
+}
+
 const ReviewsSummary = ({reviews}) => {
   return (
-    <div>
+    <div className="summary_box" style={boxStyle}>
       <h4>Reviews Summary</h4>
       <p>Reviews: {reviews.length}</p>
       <p>Average Rating: {average(reviews).toFixed(2)}</p>
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import ReviewsSummary from './ReviewsSummary.jsx';
 import axios from 'axios';
 
+const tempStyle = {
+  width: '30%',
+  display: 'inline-block',
+  marginLeft: '19%',
+  marginTop: '10px',
+  marginBottom: '10px',
+  float: 'left'
+}
+
+const wrapStyle = {
+  height: '150px',
+  width: '90%',
+  marginLeft: '5%',
+  borderTop: '1px solid #cccccc',
+  borderBottom: '1px solid #cccccc',
+}
+
+const headStyle = {
+  marginLeft: '5%',
+  borderBottom: '4px solid #f96302',
+  width: '200px'
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,8 +46,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>this is the App</h2>
+        <h2 style={headStyle}>Customer Reviews</h2>
+        <div className="wrapper" style={wrapStyle}>
         <ReviewsSummary reviews={this.state.reviews}/>
+        <div className="placeholder" style={tempStyle}><p>placeholder</p></div>
+        </div>
       </div>
     )
   }
