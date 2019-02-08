4a37bef2b92e43ccc1fb3b8cd1670c5ac5f8a1df
Trevor Smith <smittrevor@gmail.com>
2019-02-08 12:04:45 -0600

    Create GET route to retrieve all reviews for a given product_id

--- server/index.js
+++ server/index.js
@@ -9,6 +9,20 @@ const db = require(path.join(__dirname, '../database/index.js'));
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(parser.json());
 
+
+app.get('/products/:id/reviews', (req, res) => {
+  let id = req.params.id;
+
+  db.findById(id, (err, reviews) => {
+    if (err) {
+      console.log('error finding reviews by product_id')
+      res.sendStatus(400);
+    } else {
+      res.json(reviews);
+    }
+  })
+})
+
 app.post('/products/reviews', (req, res) => {
 
   db.save(req.body, (err, data) => {
