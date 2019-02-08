e79d87ad5819f04f435d3449feb0ad2440718b53
Trevor Smith <smittrevor@gmail.com>
2019-02-08 11:38:54 -0600

    Create ReviewsSummary component and render on page

---
+++ client/components/ReviewsSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ReviewsSummary = (props) => {
+  return (
+    <h4>Reviews Summary goes here</h4>
+  )
+}
+
+export default ReviewsSummary;
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import ReviewsSummary from './ReviewsSummary.jsx';
 
-const App = () => (
-  <div><h1>hello? world?</h1></div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>this is the App</h2>
+        <ReviewsSummary/>
+      </div>
+    )
+  }
+}
 
 export default App;
