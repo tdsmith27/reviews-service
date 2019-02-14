e8dd3effc19d450af004b6cd3fe8766be4ad423a
Trevor Smith <smittrevor@gmail.com>
2019-02-14 16:36:23 -0600

    More deploy testing

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './components/App.jsx';
+const path = require('path');
+import App from path.join(__dirname, './components/App.jsx');
 
 
 ReactDOM.render(<App/>, document.getElementById('app'));
